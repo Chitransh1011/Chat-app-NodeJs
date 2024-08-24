2e7363a7f354242af9b8b030b2f335b6d9368b54
Chitransh Prasanna <chitranshprasanna6666@gmail.com>
2024-08-24 16:54:55 +0530

    Added codes

--- index.js
+++ index.js
@@ -12,13 +12,15 @@ const io = socketio(server);
 io.on('connection',(socket)=>{
     console.log(`a user is connected`);
 
-    socket.on('From_client',()=>{
-        console.log("Event from coming from client")
+    socket.on('msg_send',(data)=>{
+        console.log(data);
+        // io.emit is for all the web sockets
+        //socket.emit is for that socket
+        // brodcast.emit will send message accept the original one
+        // io.emit('msg_rcvd',data)if send from one browser it will we send at every browser
+        socket.emit('msg_rcvd',data);//only send from one browser other browser will not react
     })
 
-    setInterval(()=>{
-        socket.emit('From_Server')
-    },2000)
 });
 
 app.use('/',express.static(__dirname+'/public'))
