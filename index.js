8c3c727ff0d03e7832e79de5cc0727e8ef084f1f
Chitransh Prasanna <chitranshprasanna6666@gmail.com>
2024-08-25 16:08:08 +0530

    Added chat logic

--- index.js
+++ index.js
@@ -1,31 +1,52 @@
 const express = require('express');
-
 const http = require('http');
 const socketio = require('socket.io');
 
+const connect = require('./config/database-config');
+
+const Chat = require('./models/chat');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+io.on('connection', (socket) => {
+    socket.on('join_room', (data) => {
+        console.log("joining a room", data.roomid)
+        socket.join(data.roomid);
+    });
 
-io.on('connection',(socket)=>{
-    console.log(`a user is connected`);
-
-    socket.on('msg_send',(data)=>{
+    socket.on('msg_send', async (data) => {
         console.log(data);
-        // io.emit is for all the web sockets
-        //socket.emit is for that socket
-        // brodcast.emit will send message accept the original one
-        // io.emit('msg_rcvd',data)if send from one browser it will we send at every browser
-        socket.emit('msg_rcvd',data);//only send from one browser other browser will not react
-    })
+        const chat = await Chat.create({
+            roomId: data.roomid,
+            user: data.username,
+            content: data.msg
+        });
+        io.to(data.roomid).emit('msg_rcvd', data);
+    });
 
+    socket.on('typing', (data) => {
+        socket.broadcast.to(data.roomid).emit('someone_typing');
+    })
+});
+app.set('view engine', 'ejs');
+app.use('/', express.static(__dirname + '/public'));
+
+app.get('/chat/:roomid', async (req, res) => {
+    const chats = await Chat.find({
+        roomId: req.params.roomid
+    }).select('content user');
+    console.log(chats);
+    res.render('index', {
+        name: 'Sanket',
+        id: req.params.roomid,
+        chats: chats
+    });
 });
 
-app.use('/',express.static(__dirname+'/public'))
-
-
-server.listen(3000,()=>{
-    console.log(`Server started on PORT: ${3000}`);
-})
+server.listen(3000, async () => {
+    console.log('Server started');
+    await connect();
+    console.log("mongo db connected")
+});
